f88edab527f25ce6137d4990c6cae65da8f53943
kwabsntim <daffourkwabena@gmail.com>
2026-02-04 00:42:30 +0000

    Initial commit: set up Flask application with basic routes and templates for payment processing

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/payment')
+def payment():
+    return render_template('payment.html')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
